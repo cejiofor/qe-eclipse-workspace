0b6fec8468856b7bc4cd230c98ac9e191362c589
cejiofor <cejiofor@gmail.com>
2019-09-17 23:22:12 -0500

    add files

--- TestProject/src/main/java/org/platform/testproject/Variables.java
+++ TestProject/src/main/java/org/platform/testproject/Variables.java
@@ -3,30 +3,13 @@ import java.io.*;
 import java.util.*;
 public class Variables {
 
-	/**
-	 * @param args
-	 * @throws IOException 
-	 */
-	public static void main(String[] args) throws IOException {
-		// TODO Auto-generated method stub
-		int x = 5;
-		int y = 6;
-		String myName = "Chris";
-		System.out.println(x+y+myName);
+	public static void main(String[] args) throws IOException {		
+//		ForLoops();
 		
-		int[] arr = new int[] {1,2,3,4,5};
-		System.out.println(Arrays.toString(arr));
+		// int[] Bil = {5,4,7,6,9};
+		// int[] sortedArr = BubbleSort(Bil);
 		
-		for (int i = 0; i<arr.length; i++) {
-			System.out.println("Index :" + i);
-			System.out.println(arr[i]);
-		}
-		
-		ForLoops();
-		
-		int[] Bil = {5,4,7,6,9};
-		int[] sortedArr = BubbleSort(Bil);
-		
+		// StudentScoresFile();
 		StudentScores();
 	}
 	
@@ -51,13 +34,11 @@ public class Variables {
 	
 	public static int[] BubbleSort(int[] arr) {
 		int n = arr.length;
-		int i;
-		int j;
 		int temp;
 		System.out.println("Unsorted Array: "+Arrays.toString(arr));
 		
-		for (i=1; i<= n-1; i++) {
-			for (j=n-1; j >= i; j--) {
+		for (int i=1; i<= n-1; i++) {
+			for (int j=n-1; j >= i; j--) {
 				if (arr[j]<arr[j-1]){
 					temp = arr[j];
 					arr[j] = arr[j-1];
@@ -67,43 +48,63 @@ public class Variables {
 					continue;
 			}
 		}
-
 		System.out.println("Sorted Array: "+Arrays.toString(arr));
 		return arr;
 		
 	}
 	
-	public static void StudentScores() throws IOException {
+	public static void StudentScoresFile() throws IOException {
 //		Write a Java Application that allows the user to enter 
 //		24 students scores and store them in an Array. 
 //		The application should sort the scores and print out
 //		the minimum and maximum scores.
-		
-//		Scanner StudentScore = new Scanner(System.in);
-//		File scores = new File("student_scores.txt");
-		
+
+		// Set up a random generator that will be used to generate random interger scores up to 100
 		Random rand = new Random();
+
+		// Initialize and array of length 24 to hold the student scores
 		int[] scores = new int[24];
 		
+		// Create an empty file to hold the student scores
 		File infile = new File("scores.txt");
+		// Create a printwriter object to write the student scores to a new line in the scores.txt file
 		PrintWriter scoreFile = new PrintWriter(infile);
+		
+		// Loop to add a random integer to the previously created file
 		for (int i = 0; i< 24; i++) {
 			int score = rand.nextInt(100);
-			System.out.println(score);
 			scoreFile.println(score);
+
+			// Set the score for the give index in the scores array
+			scores[i] = score ;
+		}
+
+		scoreFile.close();
+
+		// create an array of the sorted scores, sorted using the BubbleSort method
+		int[] sortedScores = BubbleSort(scores);
+
+		// Print the maximum and minimum scores from the sorted array, the first number being the least and the last number being the greatest
+		System.out.println("Max Score: "+sortedScores[0]+ " Min Score: "+sortedScores[scores.length-1]);
+	}
+
+	public static void StudentScores() {
+		// Initialize and array of length 24 to hold the student scores
+		int[] scores = new int[4];
+		
+		// Set up a scanner to accept student score input 
+		Scanner scoreQuery = new Scanner(System.in);
+
+		// Loop through to add each score input to array
+		for (int i = 0; i< scores.length; i++) {
+			System.out.printf("Enter the score for Student #%d: %n",i+1);
+			int score = scoreQuery.nextInt();  
 			scores[i] = score;
 		}
-		
-		scoreFile.close();
-		System.out.println(Arrays.toString(BubbleSort(scores)));
-		
 
-		
-		
-		
-		
-		
-		
+		// Create a new array with sorted values, then print the max and min values from the sorted array
+		int[] sortedScores = BubbleSort(scores);
+		System.out.println("Max Score: "+sortedScores[0]+ " Min Score: "+sortedScores[scores.length-1]);
 	}
 }
 
