cd96d030c3a37087effe17a31aebcb1a5e646f63
cejiofor <27887057+cejiofor@users.noreply.github.com>
2019-10-04 14:26:58 -0500

    java practice

---
+++ CoreJavaBasics/src/com/perscholas/java_basics/JavaBasicsClass.java
@@ -0,0 +1,22 @@
+package com.perscholas.java_basics;
+
+class JavaBasicsClass {
+	public static void twoInts() {
+		int i = 4;
+		int j = 5;
+		int sum = i+j;
+		System.out.println(sum);
+	}
+	
+	public static void {
+		
+	}
+	
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		twoInts();
+	}
+
+}
