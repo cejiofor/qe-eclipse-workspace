6e7b2c93705cc3dfa5c15d8ec56dd6167a29197a
cejiofor <27887057+cejiofor@users.noreply.github.com>
2019-10-14 17:00:59 -0500

    junit introduction

--- JunitFirstTest/src/test/java/com/platform/junit_intro/test/SimpleClassTest.java
+++ JunitFirstTest/src/test/java/com/platform/junit_intro/test/SimpleClassTest.java
@@ -1,16 +1,89 @@
 package com.platform.junit_intro.test;
 
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.sameInstance;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertThat;
 import static org.junit.Assert.assertTrue;
-// import org.junit.*;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 import com.platform.junit_into.SimpleClass;
-
+/*
+ * assertTrue
+ * assertFalse
+ * assertEquals
+ * assertArrayEquals
+ * assertNotNull
+ * assertNull
+ * assertSame
+ * assertNotSame
+ * assertThat
+ * */
 
 public class SimpleClassTest {
+	private static SimpleClass sc;
+	
+	@BeforeClass
+	public static void createSimpleClass() {
+		sc = new SimpleClass();
+	}
+	
 	@Test
 	public void returnTrueTest() {
 		SimpleClass sc = new SimpleClass();
-		assertTrue(sc.returnTrue());
+		boolean actual = sc.returnTrue();
+		assertTrue(actual);
 	}
+	
+	@Test
+	public void returnNullTest() {
+		SimpleClass sc = new SimpleClass();
+		Object actual = sc.returnNull();
+//		assertTrue(actual == null);
+		assertNull(actual);
+	}
+	
+	@Test
+	public void returnNotNullTest() {
+		Object actual = sc.returnNotNull();
+		assertNotNull(actual);
+	}
+	
+	@Test
+	public void returnOneTest() {
+		int actual = sc.returnOne();
+		assertTrue(actual == 1);
+	}
+	
+	@Test 
+	public void returnListTest() {
+		List<String> actual = sc.returnList();
+		List<String> expected = new ArrayList<String>();
+		expected.add("red");
+		expected.add("green");
+		expected.add("blue");
+		assertThat(actual, is(expected)); //Hamcrest
+	}
+	
+	public void returnObjectsTest() {
+		Object obj1 = sc.returnNotNull();
+		Object obj2 = sc.returnNotNull();
+		assertThat(obj1, sameInstance(obj2));
+	}
+	
+	@Test 
+	public void returnHashMapTest() {
+		Map<String,String> actual = sc.returnHashMap();
+		Map<String,String> testMap = new HashMap<String, String>();
+		
+	}
+
 }
