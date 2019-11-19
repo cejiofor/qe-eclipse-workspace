952d3d79b0c95b293fa8f165379aff7e43b90f0f
Christopher Ejiofor <cejiofor@gmail.com>
2019-11-19 10:24:48 -0600

    Spring SBA Rest controller problem

---
+++ SpringSBARestController/WebContent/WEB-INF/views/ControllerView.jsp
@@ -0,0 +1,12 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<title>Controller View</title>
+</head>
+<body>
+	<h1>@Controller View</h1>
+</body>
+</html>
--- sql/springmvc_persistence.sql
+++ sql/springmvc_persistence.sql
@@ -5,3 +5,19 @@ CREATE TABLE students (
 	email VARCHAR(45),
 	PRIMARY KEY (student_id)
 	);
+	
+DROP TABLE IF EXISTS courses;
+CREATE TABLE courses(
+	course_id INT(11) AUTO_INCREMENT,
+	code VARCHAR(45),
+	name VARCHAR(45),
+	max_size INT(11),
+	PRIMARY KEY (course_id)
+);
+
+DROP TABLE IF EXISTS course_enrollments;
+CREATE TABLE course_enrollments(
+	course_enrollement_id INT(11),
+	students_student_id INT(11),
+	courses_course_id INT(11)
+);
