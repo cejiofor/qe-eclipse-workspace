781337c4e6feee7e68db652ce2280f7e81fdc753
cejiofor <27887057+cejiofor@users.noreply.github.com>
2019-12-04 12:16:13 -0600

    Add uppercase java exercises

---
+++ JavaAlgorithmPractice/src/main/com/perscholas/exercises/UpperCase.java
@@ -0,0 +1,27 @@
+package com.perscholas.exercises;
+import java.util.Arrays;
+public class UpperCase {
+	/* Write an application with a method that takes a string with multiple words 
+	 * (such as a sentence) and capitalizes each word and returns the string.
+	 * Example: The string "red green blue cyan magenta yellow" would output:
+	 * Red Green Blue Cyan Magenta Yellow
+	 */
+	
+	public static String upperCaseSentence(String s) {
+		String[] strArr = s.split(" ");
+		for (int i=0; i<strArr.length; i++) {
+			String capitalizd = strArr[i].substring(0,1).toUpperCase() + strArr[i].substring(1);
+			strArr[i] = capitalizd;
+		}
+		
+		String newString = String.join(" ", strArr);
+		System.out.println(newString);
+		return newString;
+	}
+	
+	public static void main(String[] args) {
+		String test = "red green blue cyan magenta yellow";
+		upperCaseSentence(test);
+	}
+
+}
